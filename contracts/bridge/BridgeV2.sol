9f16d090791c90e5bc22879f9dae8930b95280c4
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-05-20 16:15:57 +0300

    DRAFT: Started Documentation

--- contracts/bridge/BridgeV2.sol
+++ contracts/bridge/BridgeV2.sol
@@ -5,4 +5,10 @@ import {Bridge} from "./Bridge.sol";
 
 import {USDCManager} from "../utils/USDCManager.sol";
 
+/**
+ * @title BridgeV2 Contract
+ *
+ * Inherits USDCManager to simplify USDC hand over process according to:
+ * https://github.com/circlefin/stablecoin-evm/blob/c582e58f691cc0cc7df1c85b6ac07267f8861520/doc/bridged_USDC_standard.md#2-ability-to-burn-locked-usdc
+ */
 contract BridgeV2 is Bridge, USDCManager {}
--- contracts/utils/USDCManager.sol
+++ contracts/utils/USDCManager.sol
@@ -3,11 +3,30 @@ pragma solidity ^0.8.9;
 
 import {IUSDCType} from "../interfaces/tokens/IUSDCType.sol";
 
+/**
+ * @title USDCManager Contract
+ *
+ * This is a utility contract, that provides enough functionality mentioned in the document to hand over control of bridging USDC contracts to Circle:
+ * https://github.com/circlefin/stablecoin-evm/blob/c582e58f691cc0cc7df1c85b6ac07267f8861520/doc/bridged_USDC_standard.md#2-ability-to-burn-locked-usdc
+ *
+ * This contracts provides minimal functionality needed to upgrade existing Brdige contract to the version of the Bridge contracts, that also inherits from
+ * this contract. Also makes it possible to upgrade a few times if needed.
+ */
 contract USDCManager {
+    /**
+     * @notice The address of the USDC token contract on the chain.
+     */
     IUSDCType public lockedUSDCAddress;
 
+    /**
+     * @notice The EOA that will be allowed to call the `burnLockedUSDC` method.
+     */
     address public managerAddress;
 
+    /**
+     * @notice The initialized version of the contract.
+     * Could be bigger than 1 if the contract is upgraded a few times.
+     */
     uint256 public initializedVersion;
 
     modifier onlyManager() {
@@ -15,6 +34,7 @@ contract USDCManager {
         _;
     }
 
+    /****/
     function __USDCManager_init(
         IUSDCType lockedUSDCAddress_,
         address managerAddress_
