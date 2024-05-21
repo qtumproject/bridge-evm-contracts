aded6807d7339a1f9db9d1694f622393ba0a1843
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-05-21 13:43:52 +0300

    Added script to calculate data for `upgradeToWithSigAndCall` function

---
+++ scripts/hardhat/calculate-bridgeV2-upgrade-data.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+import { BridgeV2__factory } from "@ethers-v6";
+
+const usdcTokenAddress = process.env.USDC_TOKEN_ADDRESS!;
+const circleTrustedAccount = process.env.CIRCLE_TRUSTED_ACCOUNT!;
+
+if(!ethers.isAddress(usdcTokenAddress!)) {
+  throw new Error("Invalid USDC_TOKEN_ADDRESS");
+}
+
+if (!ethers.isAddress(circleTrustedAccount)) {
+  throw new Error("Invalid CIRCLE_TRUSTED_ACCOUNT");
+}
+
+const initData = BridgeV2__factory.createInterface().encodeFunctionData("__USDCManager_init", [
+  usdcTokenAddress,
+  circleTrustedAccount,
+]);
+
+console.log("Data to be passed to upgradeToWithSigAndCall as data parameter: ", initData);
